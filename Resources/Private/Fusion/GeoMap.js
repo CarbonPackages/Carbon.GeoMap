5d4395569c42a1a7ff88ddedd025c94d3460f3e2
Jon Uhlmann <account@uhlmann.pro>
2024-02-11 15:58:29 +0100

    Update: Remove not used file

--- Resources/Private/Fusion/GeoMap.js
+++
@@ -1,3 +0,0 @@
-import { globalSettings, iconSettings, getMapCanvas, getAddresses, initBackend, initFrontend } from "carbon-geomap";
-
-export { globalSettings, iconSettings, getMapCanvas, getAddresses, initBackend, initFrontend };
